dcabc65e4fca3bc753e36e7f3c2da0068e2b4cf9
Rafieaydin <rafie.aydin1@gmail.com>
2024-05-11 21:37:45 +0700

    add api and runtime configuration

---
+++ backend/backend/Config/Filter.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace backend.Config
+{
+    public class Filter<T>
+    {
+
+        public Filter()
+        {
+        }
+
+        public static T filter(List<T> data, string key, int value)
+        {
+            try
+            {
+                if (data == null || data.Count == 0)
+                {
+                  throw new Exception("No data found");
+                }
+
+                if (string.IsNullOrEmpty(key))
+                {
+                    throw new Exception("Key cannot be null or empty.");
+                }
+
+                foreach (T item in data)
+                {
+                    if (Convert.ToInt16(item.GetType().GetProperty(key).GetValue(item)) == value)
+                    {
+                        return item;
+                    }
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+            return default(T);
+        }
+    
+
+
+    }
+}
---
+++ backend/backend/Request/ForumRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Request
+{
+    public class ForumRequest
+    {
+        public string title { get; set; }
+        public string content { get; set; }
+        public string created_at { get; set; }
+    }
+}
