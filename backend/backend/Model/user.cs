144aec923aa08396f01dc42d06384a5e372f1bf7
Yuinuzy <tsaqif@student.telkomuniversity.ac.id>
2024-05-15 20:34:43 +0700

    commit tsaqif

---
+++ backend/backend/Model/user.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Model
+{
+    public class user
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+
+        public string role { get; set; }
+    }
+}
+    
